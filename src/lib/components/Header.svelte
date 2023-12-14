c0220a235906d9ee673a2e75c7663a65b6bc5261
dave morgan <dave@dmorg.org>
2023-12-14 21:32:44 +0000

    second

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,40 @@
+<header>
+  <a href="/">Slow Cycling Senior...</a>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/blog2023">2023</a>
+      </li>
+
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    padding: 1rem;
+    background: lightskyblue;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  ul {
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+    gap: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+</style>
