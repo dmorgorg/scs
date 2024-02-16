3289928cc57014ccdc23b9585c7b941050cebc48
dave morgan <dave@dmorg.org>
2024-02-16 18:37:16 +0530

    tweaking

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,46 @@
+<div class="card banner">
+	<h3>slowcyclingsenior.ca</h3>
+	<h5>
+		the ramblings of an opportunistic photographer, unrepentant geek, inveterate nomad, terminal
+		cynic, opinionated curmudgeon and occasional (but&nbsp;invariably&nbsp;slow) cyclist
+	</h5>
+</div>
+
+<!-- <style lang="scss">
+	.banner {
+		align-items: center;
+		background-color: var(--lightishAccent);
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		margin: 2em 0;
+		padding: 1em 0;
+		width: 100%;
+
+		h3 {
+			color: var(--heading);
+			font-family: 'Alkes', serif;
+			font-style: italic;
+			font-weight: 600;
+			letter-spacing: 0.0375em;
+			margin: 0 auto 0.5em auto;
+			padding: 0;
+		}
+
+		h5 {
+			color: var(--text);
+			width: 80%;
+			text-align: center;
+			font-family: 'Alkes', serif;
+			font-size: 1.05em;
+			font-style: italic;
+			font-weight: normal;
+		}
+
+		@media (max-width: 768px) {
+			h5 {
+				width: 90%;
+			}
+		}
+	}
+</style> -->
