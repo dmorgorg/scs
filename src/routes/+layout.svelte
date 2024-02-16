1218aacb6fdb7eb7902c40afdd54cf070ad12ab5
dave morgan <dave@dmorg.org>
2024-02-16 18:59:40 +0530

    minor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,27 +33,6 @@
 	});
 </script>
 
-<svelte:head>
-	<!-- <link rel="stylesheet" href="/css/vars.css" />
-	<link rel="stylesheet" href="/css/root.css" />
-	<link rel="stylesheet" href="/css/fonts.css" />
-	<link rel="stylesheet" href="/css/typography.css" />
-	<link rel="stylesheet" href="/css/layout.css" />
-	<link rel="stylesheet" href="/css/components.css" />
-	<link rel="stylesheet" href="/css/header-and-footer.css" />
-	<link rel="stylesheet" href="/css/forms.css" />
-	<link rel="stylesheet" href="/css/animation.css" />
-	<link rel="stylesheet" href="/css/utilities.css" />
-	<link rel="stylesheet" href="/css/code.css" />
-	<link rel="stylesheet" href="/css/prism.css" />
-	<link
-		rel="alternate"
-		type="application/rss+xml"
-		title={siteTitle}
-		href="http://{siteURL}/api/rss.xml"
-	/> -->
-</svelte:head>
-
 <!--
 	The below markup is used on every page in the site. The <slot> is where the page's
 	actual contents will show up.
