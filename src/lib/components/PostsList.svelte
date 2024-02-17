99ffd9521c4441666c0a607d20f07f9539a2f8ad
dave morgan <dave@dmorg.org>
2024-02-17 18:33:18 +0530

    check working

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -4,7 +4,7 @@
 
 <ul class="posts-list">
 	{#each posts as post}
-		<li>
+		<li class="card top-matter">
 			<article>
 				<a href="/blog/{post.slug}">
 					<img
@@ -14,9 +14,10 @@
 						height={post.coverHeight}
 						style="ratio: {post.coverWidth} / {post.coverHeight}"
 					/>
-					<h2>
+					<h3 class="title">
 						{@html post.title}
-					</h2>
+					</h3>
+					<span class="date">&nbsp; ({post.date})</span>
 				</a>
 			</article>
 
@@ -24,3 +25,23 @@
 		</li>
 	{/each}
 </ul>
+
+<style>
+	h3.title {
+		display: inline;
+		padding-top: 2em;
+	}
+	.date {
+		font-size: 0.8em;
+		vertical-align: text-bottom;
+		/* put the date on its own line if it wants to wrap */
+		white-space: nowrap;
+	}
+	p {
+		margin-bottom: 0;
+	}
+	img {
+		margin-top: 0;
+		margin-bottom: 1em;
+	}
+</style>
