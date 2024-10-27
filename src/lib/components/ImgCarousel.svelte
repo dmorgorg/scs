e249f65f8540c7103f892f875be42e1fa986ab94
dave morgan <dave@dmorg.org>
2024-10-27 23:16:30 +0000

    stylin'

--- src/lib/components/ImgCarousel.svelte
+++ src/lib/components/ImgCarousel.svelte
@@ -74,17 +74,7 @@
 	class:black={matteTheme === 'black'}
 	transition:fade={{ duration: 500 }}
 >
-	<button class="leftChevron" on:click={getPrevious}>
-		<img src="/icons/leftChevronBrand.png" alt="left-chevron" />
-	</button>
-	<button class="rightChevron" on:click={getNext}>
-		<img src="/icons/rightChevronBrand.png" alt="right-chevron" />
-	</button>
-	<button class="x" on:click={getOut}>
-		<img src="/icons/xBrand.png" alt="long-arrow-right" />
-	</button>
-
-	<div class="buttonGroup">
+	<div class="matteButtonGroup">
 		<button class="white" on:click={() => (matteTheme = 'white')} aria-label="Set theme to white"
 		></button>
 		<button class="brand" on:click={() => (matteTheme = 'brand')} aria-label="Set theme to brand"
@@ -93,58 +83,81 @@
 		></button>
 	</div>
 
+	<button class="x" on:click={getOut}>
+		<img src="/icons/xBrand.png" alt="long-arrow-right" />
+	</button>
+
 	<div class="counter" class:black={matteTheme === 'black'}>
 		Image {currentIndex + 1} of {length}
 	</div>
 
 	<div class="inner">
-		<!-- <img transition:slide={{ duration: 500 }} src={currentFilePath} alt="x" /> -->
-		<!-- <img
-			in:fly={{ x: 200, duration: 1000 }}
-			out:fly={{ x: -200, duration: 1000 }}
-			src={currentFilePath}
-			alt="x"
-		/> -->
 		{#each ia as image, index}
 			{#if index === currentIndex}
 				<img
 					out:slide={{ duration: 500 }}
-					in:slide={{ duration: 1500, delay: 5 }}
+					in:slide={{ duration: 1200, delay: 100 }}
 					src={ia[index]}
 					alt="x"
 				/>
 			{/if}
 		{/each}
+
+		<div class="nextPrevButtons">
+			<button class="leftChevron" on:click={getPrevious}>
+				<img src="/icons/leftChevronBrand.png" alt="left-chevron" />
+			</button>
+			<button class="rightChevron" on:click={getNext}>
+				<img src="/icons/rightChevronBrand.png" alt="right-chevron" />
+			</button>
+		</div>
 	</div>
 </div>
 
 <style lang="scss">
+	// mobile first design
+
+	button {
+		background-color: white;
+		border: 2px solid var(--brand-9);
+		border-radius: var(--radius-4);
+		box-shadow: none;
+		height: 1.25rem;
+		padding: 0.125rem;
+		position: absolute;
+		width: 1.25rem;
+
+		img {
+			border: none;
+		}
+	}
+
+	.counter {
+		font-family: alkes;
+		font-style: italic;
+		font-weight: 00;
+		position: absolute;
+		bottom: 0.375rem;
+		right: 1rem;
+		transition: color 3.5s;
+	}
+
 	.matte {
 		align-items: center;
 		background-color: var(--brand-2);
 		display: flex;
+		font-size: 65%;
 		height: 100%;
 		justify-content: center;
 		left: 0;
-		padding-block-start: 2rem;
-		padding-block-end: 4rem;
-		padding-inline: 4rem;
+		padding-block-start: 1rem;
+		padding-block-end: 2rem;
+		padding-inline: 1rem;
 		position: fixed;
 		top: 0;
-		transition: background-color 1s;
+		transition: background-color 2s;
 		width: 100vw;
 		z-index: 50;
-
-		.counter {
-			font-family: alkes;
-			font-style: italic;
-			font-weight: 00;
-			position: absolute;
-			bottom: 0.5rem;
-			right: 1rem;
-			// color: black;
-			transition: color 3.5s;
-		}
 
 		&.white {
 			background-color: white;
@@ -156,100 +169,115 @@
 			background-color: black;
 			color: white;
 		}
+	}
 
-		.buttonGroup {
-			align-items: center;
-			bottom: 1rem;
-			display: flex;
-			justify-content: center;
+	.matteButtonGroup {
+		bottom: 0.75rem;
+		display: flex;
+		justify-content: center;
+		position: absolute;
 
-			position: absolute;
+		button {
+			height: 0.75rem;
+			margin-inline: 0.75rem;
+			position: relative;
+			width: 0.75rem;
 
-			button {
-				position: relative;
-				margin-inline: 0.5rem;
-				width: 1.25rem;
-				height: 1.25rem;
-				box-shadow: none;
-
-				&.brand {
-					background-color: var(--brand-2);
-				}
-				&.white {
-					background-color: white;
-				}
-				&.black {
-					background-color: black;
-				}
+			&.white {
+				background-color: white;
+			}
+			&.brand {
+				background-color: var(--brand-2);
+			}
+			&.black {
+				background-color: black;
 			}
 		}
+	}
 
-		.inner {
+	.inner {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		img {
+			border: 2px solid black;
+			margin-block: auto;
+			max-width: 100%;
+			max-height: 100%;
+			// in full screen mode in the browser, object-fit:  contain; sometimes doesn't work, rendering the border in the wrong place. Setting max width and max height instead fixes this.
+			// object-fit: contain;
+		}
+
+		.nextPrevButtons {
 			align-items: center;
 			display: flex;
-			justify-content: center;
+			justify-content: space-between;
+			margin-block-start: 1rem;
 			width: 100%;
-			height: 100%;
-
-			img {
-				border: 2px solid black;
-
-				margin-block: auto;
-				max-width: 100%;
-				max-height: 100%;
-				// in full screen mode in the browser, object-fit:  contain; sometimes doesn't work, rendering the border in the wrong place. Setting max width and max height instead fixes this.
-				// object-fit: contain;
-			}
 		}
 
-		& button {
+		button {
 			background-color: var(--bg-color);
 			background-color: white;
 			border: 2px solid var(--brand-9);
 			border-radius: var(--radius-4);
 			box-shadow: none;
-			height: 2rem;
+			height: 1.5rem;
 			padding: 0.25rem;
-			position: absolute;
-			width: 2rem;
-
-			&:hover {
-				border: 3px solid black;
-			}
+			position: relative;
+			width: 1.5rem;
 
 			img {
 				border: none;
-				padding: 0;
-				margin: 0;
-				// width: 25%;
-				height: auto;
-				max-width: 100%;
-			}
-
-			&.leftChevron {
-				left: 1rem;
-			}
-			&.rightChevron {
-				right: 1rem;
-			}
-			&.x {
-				top: 1rem;
-				right: 1rem;
 			}
 		}
+
+		// &:hover {
+		// 	border: 3px solid black;
+		// }
+
+		// img {
+		// 	border: none;
+		// 	padding: 0;
+		// 	margin: 0;
+		// 	// width: 25%;
+		// 	height: auto;
+		// 	max-width: 100%;
+		// }
+
+		// &.leftChevron {
+		// 	left: 1rem;
+		// }
+		// &.rightChevron {
+		// 	right: 1rem;
+		// }
+		// }
 	}
+
 	.matte.black button:hover {
 		border: 3px solid white;
 	}
 
-	@media (max-width: 600px) {
-		.matte {
-			font-size: 65%;
-		}
-		.buttonGroup button {
-			height: 3.25rem;
-			margin-inline: 3.5rem;
-			width: 1rem;
-		}
+	.x {
+		right: 1rem;
+		top: 1rem;
 	}
+
+	// @media (max-width: 600px) {
+	// 	.matte {
+	// 		font-size: 65%;
+	// 		padding-inline: 1rem;
+	// 	}
+
+	// 	.buttonGroup {
+	// 		button {
+	// 			bottom: -0.5rem;
+	// 			height: 1rem !important;
+	// 			width: 1rem !important;
+	// 			margin-inline: 1rem !important;
+	// 		}
+	// 	}
+	// }
 </style>
